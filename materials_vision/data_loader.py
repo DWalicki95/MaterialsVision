181761d277b34ad85db44851f0e8d1ca181d8c10
Dawid Walicki <dawidwalicki@MacBook-Air-Dawid.local>
2024-12-10 21:37:48 +0100

    mozliwosc selekcji pow. zdjec

--- materials_vision/data_loader.py
+++ materials_vision/data_loader.py
@@ -4,8 +4,8 @@ from config import DATA_PATH_DICT
 
 
 class DataLoader:
-    def __init__(self):
-        pass
+    def __init__(self, series):
+        self.data_dict = self.load_data_dict(series)
 
     def load_data_dict(self, series: str):
         if series not in ["AS", "VAB", "RL"]:
@@ -23,3 +23,14 @@ class DataLoader:
             img_paths = list(subdir.glob("*.jpg")) + list(subdir.glob("jpeg"))
             data_dict[series_name] = img_paths
         return data_dict
+
+    def keep_magnification(self, magnification: int):
+        pattern = f"_{str(magnification)}_"
+        filtered_path_list = []
+        filtered_data_dict = {}
+        for material_series, path_list in self.data_dict.items():
+            for path in path_list:
+                if pattern in path.name:
+                    filtered_path_list.append(path)
+            filtered_data_dict[material_series] = filtered_path_list
+        return filtered_data_dict
