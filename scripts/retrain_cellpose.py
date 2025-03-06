cf3d29460fc1e1c80f459c2da7272ff519610a09
Dawid Walicki <dawidwalicki@MacBookAir.T-Mobile>
2025-03-06 22:25:39 +0100

    retrain_cyto argpars fix

--- scripts/retrain_cellpose.py
+++ scripts/retrain_cellpose.py
@@ -4,8 +4,13 @@ from materials_vision.cellpose.training import retrain_cyto
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_name", type=str, default="cyto3_retrained")
     parser.add_argument("--train_dir", type=str, default=None)
     parser.add_argument("--test_dir", type=str, default=None)
+    parser.add_argument("--model_name", type=str, default="cyto3_retrained")
     args = parser.parse_args()
-    retrain_cyto(args.model_name)
+
+    retrain_cyto(
+        train_dir=args.train_dir,
+        test_dir=args.test_dir,
+        model_name=args.model_name
+    )
